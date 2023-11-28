af71afb2fdd732fea1f7910f659d1b328f0bb5b2
juanmihidgar <juanmiguelhidalgo@outlook.com>
2023-11-28 22:43:03 +0100

    Delete old routes

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('principal');
 });
-
-Route::get('/nosotros', function () {
-    return view('nosotros');
-});
-
-Route::get('/tienda', function () {
-    return view('tienda');
-});
-
-Route::get('/contacto', function () {
-    return view('contacto');
-});
