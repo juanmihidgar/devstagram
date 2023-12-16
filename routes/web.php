ad6dacc573a0e59715a375dcc6c164fa1c8db37c
juanmihidgar <juanmiguelhidalgo@outlook.com>
2023-12-16 20:24:09 +0100

    Dashboard adapted to profile with Route Model Binding User param

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,5 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 // Dashboard
-Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
+Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 
